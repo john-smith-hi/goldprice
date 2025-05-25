8b21d1adc33e1c644208ac41f239043f65c9b2ca
admin <test@admin.com>
2025-05-25 23:54:04 +0700

    254245

--- database/migrations/2025_05_22_064657_create_type_gold_table.php
+++ database/migrations/2025_05_22_064657_create_type_gold_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('type_gold', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('companies_id');
             $table->foreignId('companies_id')->constrained('companies')->onDelete('cascade');
             $table->string('name_vn')->nullable();
             $table->string('name_en')->nullable();
