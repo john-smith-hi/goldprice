04b00d1ce7acc9294ba66397e04190dda92d7fce
admin <mike841651616@gmail.comnh h smike841651616@gmail.com>
2025-05-26 08:56:31 +0700

    auto_price.php

---
+++ auto_price.php
@@ -0,0 +1,14 @@
+<?php
+use App\Http\Controllers\Auto\AutoPriceController;
+
+require __DIR__ . '/vendor/autoload.php';
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+// Khởi tạo ứng dụng
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->bootstrap();
+
+// Gọi controller
+$controller = new AutoPriceController();
+$controller->index();
+
