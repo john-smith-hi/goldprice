f62e23ce1b5f8491c4f5712dc3ef6d03af9f26f5
admin <mike841651616@gmail.comnh h smike841651616@gmail.com>
2025-05-27 09:21:07 +0700

    change footer title size

--- resources/views/user/footer.blade.php
+++ resources/views/user/footer.blade.php
@@ -3,20 +3,20 @@
     <div class="container">
       <div class="row">
         <div class="col-md-4 mb-3">
-          <h6 class="text-white">Sitemap</h6>
+          <h5 class="text-white">Sitemap</h5>
           <a href="">Trang chủ</a>
           <a href="">Giá vàng hôm nay</a>
           <a href="">Biểu đồ</a>
           <a href="">Liên hệ</a>
         </div>
         <div class="col-md-4 mb-3">
-          <h6 class="text-white">Chính sách</h6>
+          <h5 class="text-white">Chính sách</h5>
           <a href="#">Điều khoản sử dụng</a>
           <a href="#">Chính sách bảo mật</a>
           <a href="#">Giới hạn trách nhiệm</a>
         </div>
         <div class="col-md-4 mb-3">
-          <h6 class="text-white">Thông tin</h6>
+          <h5 class="text-white">Thông tin</h5>
           <a href="#">Về chúng tôi</a>
           <a href="#">Hỗ trợ</a>
           <a href="#">Góp ý</a>
