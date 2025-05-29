95faa0a9a2a7936cd5601870c552b6fd542ad7fc
admin <mike841651616@gmail.comnh h smike841651616@gmail.com>
2025-05-29 12:01:48 +0700

    add logo.png to template

--- resources/views/user/navbar.blade.php
+++ resources/views/user/navbar.blade.php
@@ -1,9 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-gold mb-4">
     <div class="container">
       <a class="navbar-brand fw-bold text-dark" href="/">
-        <h2 class="m-0" style="font-size:1.5rem;">
-          {{ Str::ucfirst(env('WEBSITE_NAME')) }}
-        </h2>
+        <img src="{{ asset('images/logo.png') }}" alt="{{ env('WEBSITE_NAME') }}" style="height: 40px; width: auto;">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
