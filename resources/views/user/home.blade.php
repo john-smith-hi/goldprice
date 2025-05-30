edfa2b1e7d6a150a1c90462aff6e4d5c6867763b
admin <test@admin.com>
2025-05-30 18:22:15 +0700

    fix price world home user blade

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -252,8 +252,8 @@
             if (json.success && json.data && json.data.length > 0) {
                 const worldGold = json.data.find(item => item.type == MAIN_TYPE_GOLD_WORLD_ID);
                 if (worldGold) {
-                    const price = worldGold.price_world;
-                    document.getElementById('world_price').innerText = priceChange.toLocaleString('vi-VN');
+                    const price_world = worldGold.price_world;
+                    document.getElementById('world_price').innerText = Math.floor(price_world / 1000).toLocaleString('vi-VN');
                 }
             }
         });
